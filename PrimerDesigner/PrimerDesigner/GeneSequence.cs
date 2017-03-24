876db709dc1d140d1d71d65a5939ce0d315eabf2
SequenceX <ich_mag@gmx.net>
2017-03-24 10:20:44 +0100

    Add new Project

---
+++ PrimerDesigner/PrimerDesigner/GeneSequence.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimerDesigner
+{
+    class GeneSequence
+    {
+        /*
+        Planed:
+        Int GS Primer:
+           DONE Check der Min&Max Länge für Genestrand 150?-1000
+           Design in Mitte der Seq
+           Try Start of Primer GG/CC/GC/CG
+           Unic Base Check
+           Primer erweitern bis Tm passt
+
+        */
+
+        //Konstruktor
+        public GeneSequence(string DNASequence)
+        {
+            this.sequence = DNASequence.ToUpper();
+            this.länge = sequence.Length;
+            if (true)
+            {
+                throw new Exception("Bad News");
+            }
+
+
+        }
+        //Getter / Setter
+        private string sequence;
+        public string Sequence
+        {
+            get
+            { return sequence; }
+            set
+            {
+                sequence = value.ToUpper();
+                länge = sequence.Length;
+            }
+        }
+        private int länge;
+        public int Länge
+        {
+            get
+            { return länge; }
+        }
+
+
+        //Methoden
+
+
+
+
+
+
+
+
+        public string[] GetGenestrandSeqPrimer(int distanceBetweenPrimer = 90, int minSeqLänge = 120, int maxSeqLänge = 1000) //Min und Max Länge für Primer mit vorgeschlagener, überschreibbarer anzeige
+        {
+            string[] strReturn = new string[2] { "", "" };
+            //Check der Min&Max Länge für Genestrand
+            if (länge < minSeqLänge || länge > maxSeqLänge)
+            {
+                return strReturn;
+            }
+
+
+
+            //strReturn[0] = "PCRF";
+            //strReturn[1] = "PCRR";
+            return strReturn;
+        }
+
+
+
+
+        public bool CheckForDnaChar()
+        {
+            int gcount = sequence.ToUpper().Count(f => f == 'G');
+            int ccount = sequence.ToUpper().Count(f => f == 'C');
+            int acount = sequence.ToUpper().Count(f => f == 'A');
+            int tcount = sequence.ToUpper().Count(f => f == 'T');
+            bool check;
+            if (gcount+ ccount+ acount+ tcount == länge)
+            {
+                check = true;
+            }
+            else
+            {
+                check = false;
+            }
+            return check;
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+    }
+
---
+++ PrimerDesigner/PrimerDesigner/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimerDesigner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Primer Primer1 = new Primer("acccctt", 50, 50);
+            Console.WriteLine("Die Tm(Nearest Neighbour)  ist: " + Primer1.GetNearestNeighbourTemp());
+            Console.WriteLine(Primer1.Sequence);
+            Console.WriteLine(Primer1.GetReverseComplement());
+
+            Console.WriteLine(" ");
+
+
+
+
+            GeneSequence seq1 = null;
+            try
+            {
+                seq1 = new GeneSequence("AAGACCgttcccggaaaGC");
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine(exc.Message);
+                Console.ReadLine();
+                Environment.Exit(0);
+
+            }
+            Console.WriteLine(seq1.Sequence);
+            //Console.WriteLine(Seq1.GetGenestrandSeqPrimer()[0]);//Output ist ein array. [0] wird ausgegeben =Primer F
+            // Console.WriteLine( Seq1.GetGenestrandSeqPrimer()[1]);//Output ist ein array. [1] wird ausgegeben =Primer R
+            //Console.WriteLine(Seq1.CheckForDnaChar());
+            Console.WriteLine("ende");
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
