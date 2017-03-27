7734a8d131dc5c71e63da73bb07cf6ea76be71a4
SequenceXw <ich_mag@gmx.net>
2017-03-27 15:16:19 +0200

    Fixed Bug in Rev Primer

--- PrimerDesigner/PrimerDesigner/GeneSequence.cs
+++ PrimerDesigner/PrimerDesigner/GeneSequence.cs
@@ -252,6 +252,7 @@ namespace PrimerDesigner
                 nextRPrimerCounter++;
             }
             //Return R Primer
+            rPrimer.Sequence = rPrimer.GetReverseComplement();
             strReturn[1] = rPrimer.Sequence;
             return strReturn;
         }
