9d08bb38fd05430e05a0f7d2c9fcfbd2cda79031
SequenceX <ich_mag@gmx.net>
2017-03-27 21:53:00 +0200

    Primer Creator Page Done

--- PrimerDesigner/PrimerDesignerGUI/Form1.Designer.cs
+++ PrimerDesigner/PrimerDesignerGUI/Form1.Designer.cs
@@ -32,6 +32,8 @@
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.listBox2 = new System.Windows.Forms.ListBox();
             this.button1 = new System.Windows.Forms.Button();
             this.tabPage2 = new System.Windows.Forms.TabPage();
             this.label6 = new System.Windows.Forms.Label();
@@ -47,6 +49,8 @@
             this.label1 = new System.Windows.Forms.Label();
             this.richTextBox2 = new System.Windows.Forms.RichTextBox();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.richTextBox7 = new System.Windows.Forms.RichTextBox();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.tabPage2.SuspendLayout();
@@ -56,10 +60,11 @@
             // 
             this.listBox1.AllowDrop = true;
             this.listBox1.FormattingEnabled = true;
+            this.listBox1.HorizontalScrollbar = true;
             this.listBox1.ItemHeight = 16;
-            this.listBox1.Location = new System.Drawing.Point(3, 6);
+            this.listBox1.Location = new System.Drawing.Point(97, 6);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(213, 180);
+            this.listBox1.Size = new System.Drawing.Size(213, 212);
             this.listBox1.TabIndex = 0;
             // 
             // tabControl1
@@ -70,33 +75,57 @@
             this.tabControl1.Location = new System.Drawing.Point(1, 0);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(230, 274);
+            this.tabControl1.Size = new System.Drawing.Size(322, 332);
             this.tabControl1.TabIndex = 1;
             // 
             // tabPage1
             // 
+            this.tabPage1.Controls.Add(this.progressBar1);
+            this.tabPage1.Controls.Add(this.listBox2);
             this.tabPage1.Controls.Add(this.button1);
             this.tabPage1.Controls.Add(this.listBox1);
             this.tabPage1.Location = new System.Drawing.Point(4, 25);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(222, 245);
+            this.tabPage1.Size = new System.Drawing.Size(314, 303);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Primer Creator";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(7, 266);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(300, 31);
+            this.progressBar1.Step = 1;
+            this.progressBar1.TabIndex = 3;
+            // 
+            // listBox2
+            // 
+            this.listBox2.AllowDrop = true;
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.HorizontalScrollbar = true;
+            this.listBox2.ItemHeight = 16;
+            this.listBox2.Location = new System.Drawing.Point(7, 6);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(73, 212);
+            this.listBox2.TabIndex = 2;
+            // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft YaHei", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(3, 205);
+            this.button1.Location = new System.Drawing.Point(7, 223);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(213, 37);
+            this.button1.Size = new System.Drawing.Size(300, 37);
             this.button1.TabIndex = 1;
             this.button1.Text = "Create Primer";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // tabPage2
             // 
+            this.tabPage2.Controls.Add(this.label7);
+            this.tabPage2.Controls.Add(this.richTextBox7);
             this.tabPage2.Controls.Add(this.label6);
             this.tabPage2.Controls.Add(this.richTextBox6);
             this.tabPage2.Controls.Add(this.label5);
@@ -113,7 +142,7 @@
             this.tabPage2.Location = new System.Drawing.Point(4, 25);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(222, 245);
+            this.tabPage2.Size = new System.Drawing.Size(314, 303);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Settings";
             this.tabPage2.UseVisualStyleBackColor = true;
@@ -122,7 +151,7 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft YaHei", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(51, 176);
+            this.label6.Location = new System.Drawing.Point(94, 173);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(105, 17);
             this.label6.TabIndex = 13;
@@ -131,7 +160,7 @@
             // richTextBox6
             // 
             this.richTextBox6.Font = new System.Drawing.Font("Microsoft YaHei", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.richTextBox6.Location = new System.Drawing.Point(164, 173);
+            this.richTextBox6.Location = new System.Drawing.Point(207, 170);
             this.richTextBox6.Name = "richTextBox6";
             this.richTextBox6.Size = new System.Drawing.Size(52, 26);
             this.richTextBox6.TabIndex = 12;
@@ -141,7 +170,7 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft YaHei", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(54, 144);
+            this.label5.Location = new System.Drawing.Point(97, 141);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(102, 17);
             this.label5.TabIndex = 11;
@@ -150,7 +179,7 @@
             // richTextBox5
             // 
             this.richTextBox5.Font = new System.Drawing.Font("Microsoft YaHei", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.richTextBox5.Location = new System.Drawing.Point(164, 141);
+            this.richTextBox5.Location = new System.Drawing.Point(207, 138);
             this.richTextBox5.Name = "richTextBox5";
             this.richTextBox5.Size = new System.Drawing.Size(52, 26);
             this.richTextBox5.TabIndex = 10;
@@ -160,7 +189,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft YaHei", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(75, 112);
+            this.label3.Location = new System.Drawing.Point(118, 109);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(81, 17);
             this.label3.TabIndex = 9;
@@ -170,7 +199,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft YaHei", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(1, 80);
+            this.label4.Location = new System.Drawing.Point(44, 77);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(155, 17);
             this.label4.TabIndex = 8;
@@ -179,7 +208,7 @@
             // richTextBox3
             // 
             this.richTextBox3.Font = new System.Drawing.Font("Microsoft YaHei", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.richTextBox3.Location = new System.Drawing.Point(165, 109);
+            this.richTextBox3.Location = new System.Drawing.Point(208, 106);
             this.richTextBox3.Name = "richTextBox3";
             this.richTextBox3.Size = new System.Drawing.Size(52, 26);
             this.richTextBox3.TabIndex = 7;
@@ -188,7 +217,7 @@
             // richTextBox4
             // 
             this.richTextBox4.Font = new System.Drawing.Font("Microsoft YaHei", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.richTextBox4.Location = new System.Drawing.Point(164, 77);
+            this.richTextBox4.Location = new System.Drawing.Point(207, 74);
             this.richTextBox4.Name = "richTextBox4";
             this.richTextBox4.Size = new System.Drawing.Size(52, 26);
             this.richTextBox4.TabIndex = 6;
@@ -198,7 +227,7 @@
             // 
             this.button2.Enabled = false;
             this.button2.Font = new System.Drawing.Font("Microsoft YaHei", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(3, 205);
+            this.button2.Location = new System.Drawing.Point(46, 261);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(214, 36);
             this.button2.TabIndex = 5;
@@ -209,7 +238,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft YaHei", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(35, 48);
+            this.label2.Location = new System.Drawing.Point(78, 45);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(121, 17);
             this.label2.TabIndex = 4;
@@ -219,7 +248,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(18, 16);
+            this.label1.Location = new System.Drawing.Point(61, 13);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(138, 17);
             this.label1.TabIndex = 3;
@@ -228,7 +257,7 @@
             // richTextBox2
             // 
             this.richTextBox2.Font = new System.Drawing.Font("Microsoft YaHei", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.richTextBox2.Location = new System.Drawing.Point(165, 45);
+            this.richTextBox2.Location = new System.Drawing.Point(208, 42);
             this.richTextBox2.Name = "richTextBox2";
             this.richTextBox2.Size = new System.Drawing.Size(52, 26);
             this.richTextBox2.TabIndex = 1;
@@ -237,17 +266,36 @@
             // richTextBox1
             // 
             this.richTextBox1.Font = new System.Drawing.Font("Microsoft YaHei", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.richTextBox1.Location = new System.Drawing.Point(165, 13);
+            this.richTextBox1.Location = new System.Drawing.Point(208, 10);
             this.richTextBox1.Name = "richTextBox1";
             this.richTextBox1.Size = new System.Drawing.Size(52, 26);
             this.richTextBox1.TabIndex = 0;
             this.richTextBox1.Text = "50";
             // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft YaHei", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(91, 208);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(108, 17);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Output Directory:";
+            // 
+            // richTextBox7
+            // 
+            this.richTextBox7.Font = new System.Drawing.Font("Microsoft YaHei", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox7.Location = new System.Drawing.Point(47, 229);
+            this.richTextBox7.Name = "richTextBox7";
+            this.richTextBox7.Size = new System.Drawing.Size(213, 26);
+            this.richTextBox7.TabIndex = 14;
+            this.richTextBox7.Text = "GET DESKTOP PFAD";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(231, 275);
+            this.ClientSize = new System.Drawing.Size(325, 336);
             this.Controls.Add(this.tabControl1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -262,8 +310,6 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.TabControl tabControl1;
         private System.Windows.Forms.TabPage tabPage1;
         private System.Windows.Forms.TabPage tabPage2;
@@ -281,6 +327,11 @@
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.RichTextBox richTextBox6;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.RichTextBox richTextBox7;
     }
 }
 
--- PrimerDesigner/PrimerDesignerGUI/Form1.cs
+++ PrimerDesigner/PrimerDesignerGUI/Form1.cs
@@ -19,6 +19,10 @@ namespace PrimerDesignerGUI
            System.Windows.Forms.DragEventHandler(this.listBox1_DragDrop);
             this.listBox1.DragEnter += new
            System.Windows.Forms.DragEventHandler(this.listBox1_DragEnter);
+            this.listBox2.DragDrop += new
+           System.Windows.Forms.DragEventHandler(this.listBox2_DragDrop);
+            this.listBox2.DragEnter += new
+           System.Windows.Forms.DragEventHandler(this.listBox2_DragEnter);
         }
         private void listBox1_DragEnter(object sender, System.Windows.Forms.DragEventArgs e)
         {
@@ -30,9 +34,80 @@ namespace PrimerDesignerGUI
         private void listBox1_DragDrop(object sender, System.Windows.Forms.DragEventArgs e)
         {
             string[] s = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+            string[] s2 = new string[s.Length];
             int i;
             for (i = 0; i < s.Length; i++)
+            {
+                try
+                {
+                    s2[i] = s[i].Substring(s[i].LastIndexOf(@"\") + 1, s[i].LastIndexOf(@"-") - s[i].LastIndexOf(@"\") - 1);
+                }
+                catch (Exception)
+                {
+                    s2[i] = Microsoft.VisualBasic.Interaction.InputBox( "Genname wurde nicht automatisch erkannt", "Genname fehlt", "Genname");
+                }
+                listBox2.Items.Add(s2[i]);
                 listBox1.Items.Add(s[i]);
+            }
+        }
+        private void listBox2_DragEnter(object sender, System.Windows.Forms.DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.All;
+            else
+                e.Effect = DragDropEffects.None;
+        }
+        private void listBox2_DragDrop(object sender, System.Windows.Forms.DragEventArgs e)
+        {
+            string[] s = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+            string[] s2 = new string[s.Length];
+            int i;
+            for (i = 0; i < s.Length; i++)
+            {
+                try
+                {
+                    s2[i] = s[i].Substring(s[i].LastIndexOf(@"\") + 1, s[i].LastIndexOf(@"-") - s[i].LastIndexOf(@"\") - 1);
+                }
+                catch (Exception)
+                {
+                    s2[i] = Microsoft.VisualBasic.Interaction.InputBox("Genname wurde nicht automatisch erkannt", "Genname fehlt", "Genname");
+                }
+                listBox2.Items.Add(s2[i]);
+                listBox1.Items.Add(s[i]);
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int i=0;
+            var path = new List<string>();
+            var geneName = new List<string>();
+            foreach (object inlistbox in listBox1.Items)
+            {
+                //MessageBox.Show(inlistbox.ToString());
+                path.Add(inlistbox.ToString());
+            }
+            foreach (object inlistbox in listBox2.Items)
+            {
+                //MessageBox.Show(inlistbox.ToString());
+                geneName.Add(inlistbox.ToString());
+            }
+            var pathArray = path.ToArray();
+            var geneNameArray = geneName.ToArray();
+            if (geneNameArray.Length==0)
+            {
+                MessageBox.Show("Es wurden keine Sequenzen ausgewählt.");
+                goto Ende;
+            } 
+            progressBar1.Maximum = pathArray.Length;
+            progressBar1.Value = 0;
+            while (i < pathArray.Length)
+            {
+
+                //HIER ERSTELLUNG DER PRIMER EINFÜGEN
+                progressBar1.Value= progressBar1.Value+1;
+                i++;
+            }
+        Ende:;
         }
     }
 }
