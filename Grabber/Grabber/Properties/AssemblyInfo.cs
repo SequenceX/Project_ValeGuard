05e8a0010d1baf89ab599dccb8d21cb7c34fb021
SequenceXw <ich_mag@gmx.net>
2017-05-04 14:03:27 +0200

    Start Grabber Project

--- Grabber/Grabber/Properties/AssemblyInfo.cs
+++ Grabber/Grabber/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Allgemeine Informationen über eine Assembly werden über die folgenden 
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die einer Assembly zugeordnet sind.
-[assembly: AssemblyTitle("PrimerDesigner")]
+[assembly: AssemblyTitle("Grabber")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("PrimerDesigner")]
+[assembly: AssemblyProduct("Grabber")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
-[assembly: Guid("ce8573af-8abb-4211-8fe7-d8ad84b66f41")]
+[assembly: Guid("f1db19ab-9a69-4679-9b57-23df0ecb2be8")]
 
 // Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
 //
